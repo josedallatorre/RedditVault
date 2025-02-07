acbf42be6958ccbcb3ca6d14a95f2380530a2cca
josedallatorre <jose.dallatorre@hotmail.com>
2025-02-07 18:05:23 +0000

    feat: seed initialize

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using RazorPagesPost.Data;
+using RedditVault.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +10,13 @@ builder.Services.AddDbContext<RazorPagesPostContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("RazorPagesPostContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesPostContext' not found.")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
