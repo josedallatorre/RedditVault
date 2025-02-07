1e37bb4306522f832d6b7f47497815cbebedaaac
josedallatorre <jose.dallatorre@hotmail.com>
2025-02-07 18:39:48 +0000

    style: better field name

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RedditVault.Models;
 
@@ -6,6 +7,7 @@ public class Post
 {
     public int Id { get; set; }
     public string? Title { get; set; }
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime Date { get; set; }
 }
