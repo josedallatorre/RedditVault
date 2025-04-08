36072e287e8ccbc6729529fe6b8c9a26e582f1a7
josedallatorre <jose.dallatorre@hotmail.com>
2025-04-08 18:56:39 +0200

    feat: hardcoded download images

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 using RazorPagesPost.Data;
 using RedditVault.Models;
 var builder = WebApplication.CreateBuilder(args);
@@ -41,14 +42,36 @@ client.DefaultRequestHeaders.Add("User-Agent","RedditTest/1.0");
 var request = new HttpRequestMessage(HttpMethod.Get, "https://oauth.reddit.com/user/33prova33/saved");
 request.Headers.Add("Authorization", $"Bearer {accessToken}");
 var response = await client.SendAsync(request);
-if (response.IsSuccessStatusCode)
-{
-    Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+if (response.IsSuccessStatusCode){
+    var jsonString = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(JsonConvert.DeserializeObject<object>(jsonString));
     Console.WriteLine("bye");   
+    // URL of the file to be downloaded
+    var fileUrl = "https://i.redd.it/o9sflaranlte1.jpeg";
+
+    // Send a GET request to the specified Uri
+    using (var r = await client.GetAsync(fileUrl, HttpCompletionOption.ResponseHeadersRead))
+        {
+            r.EnsureSuccessStatusCode(); // Throw if not a success code.
+
+            // Path to save the file
+            var filePath = Path.Combine(Environment.CurrentDirectory, "mage.jpeg");
+
+            // Read the content into a MemoryStream and then write to file
+            using (var ms = await r.Content.ReadAsStreamAsync())
+            using (var fs = File.Create(filePath))
+            {
+                await ms.CopyToAsync(fs);
+                fs.Flush();
+            }
+    }
+
 }
 else
 {
     Console.WriteLine(response.ToString());
 }
+        
 
 app.Run();
