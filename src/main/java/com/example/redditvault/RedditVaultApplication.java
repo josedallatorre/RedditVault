98dfc98a2d1c110ca2dbccd529cb09319f967dc8
josedallatorre <jose.dallatorre@hotmail.com>
2025-04-09 16:49:29 +0200

    feat: first time working with spring boot

--- src/main/java/com/example/redditvault/RedditVaultApplication.java
+++ src/main/java/com/example/redditvault/RedditVaultApplication.java
@@ -2,12 +2,21 @@ package com.example.redditvault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class RedditVaultApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RedditVaultApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RedditVaultApplication.class, args);
+    }
+    @GetMapping
+    public List<String> hello() {
+        return List.of("Hello World", "asj");
+    }
 
 }
