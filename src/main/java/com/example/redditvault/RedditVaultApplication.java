9acec45df8abe1b4d5ccd810edee576ccdd07b42
josedallatorre <jose.dallatorre@hotmail.com>
2025-04-09 15:55:07 +0200

    feat: rewriting in java spring boot

---
+++ src/main/java/com/example/redditvault/RedditVaultApplication.java
@@ -0,0 +1,13 @@
+package com.example.redditvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditVaultApplication.class, args);
+	}
+
+}
