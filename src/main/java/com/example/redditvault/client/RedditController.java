ce68b7fe00595e5482e03ed65e30993809160d80
josedallatorre <jose.dallatorre@hotmail.com>
2025-04-16 18:07:10 +0200

    feat: starting oauth process

--- src/main/java/com/example/redditvault/client/RedditController.java
+++ src/main/java/com/example/redditvault/client/RedditController.java
@@ -1,12 +1,17 @@
 package com.example.redditvault.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
-@RequestMapping(path = "api/v1/me")
 public class RedditController {
     private final RedditClientService redditClientService;
 
@@ -15,8 +20,16 @@ public class RedditController {
         this.redditClientService = redditClientService;
     }
 
-     @GetMapping
-    public String getRedditClient() {
+     @GetMapping(path = "api/v1/me")
+    public ResponseEntity<String> getRedditClient(Model model) {
         return redditClientService.getMe();
+     }
+
+     @GetMapping(path = "/auth")
+    public ResponseEntity<Void> getAuth(Model model) {
+         URI redditAuthUri = URI.create(RedditClientService.getAuth());
+         HttpHeaders headers = new HttpHeaders();
+         headers.setLocation(redditAuthUri);
+         return new ResponseEntity<>(headers, HttpStatus.FOUND);
      }
 }
