d436bd2171a8959cc1ec1e16a919d1038d500abc
josedallatorre <jose.dallatorre@hotmail.com>
2025-04-23 19:58:31 +0200

    fix: unused import

--- src/main/java/com/example/redditvault/client/RedditController.java
+++ src/main/java/com/example/redditvault/client/RedditController.java
@@ -1,19 +1,11 @@
 package com.example.redditvault.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.Authenticator;
-import java.net.HttpURLConnection;
-import java.net.PasswordAuthentication;
-import java.net.URI;
-import java.net.http.HttpClient;
 
 @RestController
 public class RedditController {
