4771b6cd88ac7cd0d61db79b589db6883ec924ef
josedallatorre <jose.dallatorre@hotmail.com>
2025-04-24 22:41:15 +0200

    feat: adding class to define reddit json response structure 6

---
+++ src/main/java/com/example/redditvault/client/RedditSavedItem.java
@@ -0,0 +1,52 @@
+package com.example.redditvault.client;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RedditSavedItem {
+    private String title;
+    private String selftext;
+    private String subreddit;
+    private String author_fullname;
+    private boolean saved;
+
+    // Getters and setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSelftext() {
+        return selftext;
+    }
+
+    public void setSelftext(String selftext) {
+        this.selftext = selftext;
+    }
+
+    public String getSubreddit() {
+        return subreddit;
+    }
+
+    public void setSubreddit(String subreddit) {
+        this.subreddit = subreddit;
+    }
+
+    public String getAuthor_fullname() {
+        return author_fullname;
+    }
+
+    public void setAuthor_fullname(String author_fullname) {
+        this.author_fullname = author_fullname;
+    }
+
+    public boolean isSaved() {
+        return saved;
+    }
+
+    public void setSaved(boolean saved) {
+        this.saved = saved;
+    }
+}
