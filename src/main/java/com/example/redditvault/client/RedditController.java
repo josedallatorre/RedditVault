ddafcbbe95c1173a91d1e8d428fe8b214d7f9763
josedallatorre <jose.dallatorre@hotmail.com>
2025-04-28 23:47:10 +0200

    feat: adding route to download medias

--- src/main/java/com/example/redditvault/client/RedditController.java
+++ src/main/java/com/example/redditvault/client/RedditController.java
@@ -2,10 +2,12 @@ package com.example.redditvault.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
 
 @RestController
 public class RedditController {
@@ -42,6 +44,20 @@ public class RedditController {
         String token = bearerToken.replace("Bearer ", "").trim();
         return redditClientService.getUserSaved(token);
         //return ResponseEntity.ok(userJson);
+    }
+    @PostMapping("download")
+    public void downloadRedditMedia(@RequestBody String urlStr, String file) throws IOException {
+        URL url = new URL(urlStr);
+        BufferedInputStream bis = new BufferedInputStream(url.openStream());
+        FileOutputStream fis = new FileOutputStream(file);
+        byte[] buffer = new byte[1024];
+        int count=0;
+        while((count = bis.read(buffer,0,1024)) != -1)
+        {
+            fis.write(buffer, 0, count);
+        }
+        fis.close();
+        bis.close();
     }
 
 }
