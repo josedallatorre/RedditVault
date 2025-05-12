500ee0f013398ad091ffb7a76718b5cd148a0c59
josedallatorre <jose.dallatorre@hotmail.com>
2025-05-13 00:10:20 +0200

    feat: added option for video

--- src/main/java/com/example/redditvault/client/RedditMedia.java
+++ src/main/java/com/example/redditvault/client/RedditMedia.java
@@ -1,4 +1,16 @@
 package com.example.redditvault.client;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RedditMedia {
+    private RedditVideo reddit_video;
+
+    public RedditVideo getReddit_video() {
+        return reddit_video;
+    }
+
+    public void setReddit_video(RedditVideo reddit_video) {
+        this.reddit_video = reddit_video;
+    }
 }
