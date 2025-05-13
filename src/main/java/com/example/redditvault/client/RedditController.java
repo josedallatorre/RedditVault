365987218476c723e9a156bd1c8d4c8e3e5b8715
josedallatorre <jose.dallatorre@hotmail.com>
2025-05-13 03:24:50 +0200

    feat: downloading video from post

--- src/main/java/com/example/redditvault/client/RedditController.java
+++ src/main/java/com/example/redditvault/client/RedditController.java
@@ -1,12 +1,12 @@
 package com.example.redditvault.client;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-
 
 @RestController
 public class RedditController {
@@ -59,9 +59,13 @@ public class RedditController {
         ).join();
     }
 
-    @GetMapping("/scrape")
-    public String scrapeAndDownload(@RequestParam String postUrl) throws IOException {
-        List<DownloadRequest> mediaItems = redditClientService.scrapeMediaFromPost(postUrl);
+    @PostMapping("/scrape")
+    public String scrapeAndDownload(@RequestBody String postUrl) throws IOException {
+        System.out.println(postUrl);
+        ObjectMapper mapper = new ObjectMapper();
+        Post post = mapper.readValue(postUrl, Post.class);
+        System.out.println(post.getUrl());
+        List<DownloadRequest> mediaItems = redditClientService.scrapeMediaFromPost(post.getUrl());
 
         for (DownloadRequest item : mediaItems) {
             redditClientService.download(item.getUrl(), item.getFilename());
