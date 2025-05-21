d848c58c16981dbfa98942d749503310396e38a2
josedallatorre <jose.dallatorre@hotmail.com>
2025-05-22 00:15:13 +0200

    feat: route to requested page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Profile from './Pages/Profile';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="Reddit-auth-link"
-          href="http://localhost:8080/auth"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Login with Reddit
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={< Home />} />
+      <Route path="/profile" element={< Profile />} />
+    </Routes>
+    </>
   );
 }
 
