7596ad7d8818c74e4519034ba5c572e10902bbb2
josedallatorre <jose.dallatorre@hotmail.com>
2025-05-22 00:15:51 +0200

    feat: moved Home to page to another folder

---
+++ frontend/src/Pages/Home.tsx
@@ -0,0 +1,33 @@
+import logo from '../logo.svg';
+import '../App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a
+          className="Reddit-auth-link"
+          href="http://localhost:8080/auth"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Login with Reddit
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
