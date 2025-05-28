f8e9b9d083496722d01fafaab70dd4c49997c487
josedallatorre <jose.dallatorre@hotmail.com>
2025-05-28 23:56:52 +0200

    feat: using layout as basic page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Profile from './Pages/Profile';
 import Home from './Pages/Home';
 import SavedPost from './Pages/SavedPost';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <>
-    <Routes>
-      <Route path="/" element={< Home />} />
-      <Route path="/profile" element={< Profile />} />
-      <Route path="/saved" element={< SavedPost />} />
-    </Routes>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={< Layout />} />
+        <Route index element={<Home />} />
+        <Route path="/profile" element={< Profile />} />
+        <Route path="/saved" element={< SavedPost />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
