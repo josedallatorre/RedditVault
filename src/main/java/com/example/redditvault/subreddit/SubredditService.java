8a88377a4901dea6a8f250cb433c19af40a81d45
josedallatorre <jose.dallatorre@hotmail.com>
2025-06-04 18:01:12 +0200

    feat: basic service for Subreddit

---
+++ src/main/java/com/example/redditvault/subreddit/SubredditService.java
@@ -0,0 +1,39 @@
+package com.example.redditvault.subreddit;
+
+import com.example.redditvault.subreddit.SubredditRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SubredditService {
+    private final SubredditRepository subredditRepository;
+
+    @Autowired
+    public SubredditService(SubredditRepository subredditRepository){
+        this.subredditRepository = subredditRepository;
+    }
+
+    public List<Subreddit> getAllSubreddits(){
+        return subredditRepository.findAll();
+    }
+
+    public void addNewSubreddit(Subreddit subreddit) {
+        Optional<Subreddit> subredditOptional =  subredditRepository.findsubredditByName(subreddit.getName());
+        if (subredditOptional.isPresent()) {
+            throw new IllegalStateException("Subreddit name already exists");
+        }
+        subredditRepository.save(subreddit);
+        System.out.println(subreddit);
+    }
+
+    public void deleteSubreddit(String subredditName) {
+        boolean exists = subredditRepository.existsById(subredditName);
+        if (!exists) {
+            throw new IllegalStateException("Subreddit with name "+ subredditName + " does not exist");
+        }
+        subredditRepository.deleteById(subredditName);
+    }
+}
