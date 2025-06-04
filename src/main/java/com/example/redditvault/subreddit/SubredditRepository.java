f605efb67d2780bba1cc64429571137444dbe15a
josedallatorre <jose.dallatorre@hotmail.com>
2025-06-04 18:00:16 +0200

    feat: basic repository for Subreddit

---
+++ src/main/java/com/example/redditvault/subreddit/SubredditRepository.java
@@ -0,0 +1,11 @@
+package com.example.redditvault.subreddit;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SubredditRepository extends JpaRepository<Subreddit, String> {
+    Optional<Subreddit> findsubredditByName(String subredditName);
+}
