93c85b3c08de3ce1209547eebc3308dabffd1780
josedallatorre <jose.dallatorre@hotmail.com>
2025-06-12 23:59:43 +0200

    style:adding pagination

--- frontend/src/Pages/Subreddit.tsx
+++ frontend/src/Pages/Subreddit.tsx
@@ -70,6 +70,21 @@ function Subreddit() {
                     )
                 )
             }
+            import * as React from 'react';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+
+export default function BasicPagination() {
+  return (
+    <Stack spacing={2}>
+      <Pagination count={10} />
+      <Pagination count={10} color="primary" />
+      <Pagination count={10} color="secondary" />
+      <Pagination count={10} disabled />
+    </Stack>
+  );
+}
+
         </div>
         /*
         <div className="max-w-3xl mx-auto my-5 p-5 bg-gray-50 rounded-xl shadow-md font-sans">
