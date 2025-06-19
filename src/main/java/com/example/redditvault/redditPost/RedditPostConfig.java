f64804ec51853ac7d7068b30b05d9ca792b734a2
josedallatorre <jose.dallatorre@hotmail.com>
2025-06-19 23:26:35 +0200

    fix: adjust to new structure

--- src/main/java/com/example/redditvault/redditPost/RedditPostConfig.java
+++ src/main/java/com/example/redditvault/redditPost/RedditPostConfig.java
@@ -11,8 +11,8 @@ public class RedditPostConfig {
     @Bean
     CommandLineRunner commandLineRunner(RedditPostRepository repository) {
         return args -> {
-            RedditPost test1 = new RedditPost("1","test33",  "title1");
-            RedditPost test2 = new RedditPost("2", "test35", "title2");
+            RedditPost test1 = new RedditPost("1","test33",  "title1", "url1");
+            RedditPost test2 = new RedditPost("2", "test35", "title2", "url2");
             repository.saveAll(List.of(test1,test2));
         };
     }
