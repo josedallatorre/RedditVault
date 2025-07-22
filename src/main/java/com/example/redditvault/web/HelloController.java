64007dea696dae7b74f595f429d9fc72b71eba4f
josedallatorre <jose.dallatorre@hotmail.com>
2025-07-22 17:17:06 +0200

    feat: basic controller to verify secure endpoints

--- src/main/java/com/example/redditvault/web/HelloController.java
+++ src/main/java/com/example/redditvault/web/HelloController.java
@@ -1,16 +1,26 @@
 package com.example.redditvault.web;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-
+@RequestMapping("/api/v1/auth")
 @RestController
 public class HelloController {
+    private final AuthenticationService authenticationService;
 
-    @GetMapping("/")
-    public String hello(Authentication authentication) {
-        return "Hello, " + authentication.getName() + "!";
+    public HelloController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
     }
 
 }
