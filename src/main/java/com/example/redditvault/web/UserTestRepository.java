b2c41ce6c7c328f7c8b33612517a3850b7670259
josedallatorre <jose.dallatorre@hotmail.com>
2025-07-22 17:19:13 +0200

    feat: temp user repo for jwt structure

---
+++ src/main/java/com/example/redditvault/web/UserTestRepository.java
@@ -0,0 +1,9 @@
+package com.example.redditvault.web;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserTestRepository extends JpaRepository<UserTest, Integer> {
+    Optional<UserTest> findByEmail(String email);
+}
