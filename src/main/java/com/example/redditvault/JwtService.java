e73d8eb5d7832aa6e348b9b0f5d37d595df666f1
josedallatorre <jose.dallatorre@hotmail.com>
2025-07-24 02:57:59 +0200

    feat: keys2

--- src/main/java/com/example/redditvault/JwtService.java
+++ src/main/java/com/example/redditvault/JwtService.java
@@ -1,27 +1,35 @@
 package com.example.redditvault;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
-
 import java.security.Key;
-import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
 @Service
 public class JwtService {
 
-    private static String SECRET_KEY = "UVplVkcrcDhRRkQ4M2E4WGZnbXZuU25SUjRWak9pYzc0RHVOYkY4K3VDTWdXYnM3bjNRU3dDeGYvSVNJY2dnK25CNWVFSjFnNnkyT1Y4RWY4WHZISC9SUU5PQmYrOXpBc3R2bFpFaWQxTnZrSFlGeXFBNFhpT01qdktOYmtHd29WYlQ1Q3Izck1mbUdXZjBxeHptRFEvWnFwSnRBcW9XcW16Ym9NeU9CcWJ1bkVhUE9seUp1Nk1XUnRGNmFSUEE2dHdzUmM1aVkxRzBpcUpXNTJXTHlWbGRsVEJyaTBjU1FXdmFkWmhDd3lrYUtEdGM3ZWtoNVE3TEJ3Sm9IQUpYZTBpTHVzUEpGWDRkaTF2dDk3VkxBTW5KZVUvSWVUdUloWGZGQXNaV0RFV3ZBaTVLcjI4MS84bDFKVjkrZmZjNXBFeTFHWHd0YjBPb1BvZjhia2xHbmtpQjFlVkFjYlVqNkMvRHpuQ1pVVDFJPQ==";
-    public String extractUsername(String token){
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
+    @Value("${application.security.jwt.refresh-token.expiration}")
+    private long refreshExpiration;
+
+    public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
@@ -29,31 +37,49 @@ public class JwtService {
     public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
     }
-    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return buildToken(extraClaims, userDetails, jwtExpiration);
+    }
+
+    public String generateRefreshToken(
+            UserDetails userDetails
+    ) {
+        return buildToken(new HashMap<>(), userDetails, refreshExpiration);
+    }
+
+    private String buildToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails,
+            long expiration
+    ) {
         return Jwts
                 .builder()
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() +100 + 60*60*1000))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
-        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 
-    public boolean isTokenExpired(String token) {
+    private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
 
-    public Date extractExpiration(String token){
+    private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
-    private Claims extractAllClaims(String token){
+    private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
                 .setSigningKey(getSignInKey())
@@ -63,7 +89,7 @@ public class JwtService {
     }
 
     private Key getSignInKey() {
-        byte keyBytes[] = Base64.getDecoder().decode(SECRET_KEY);
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
