7b720d22574bb8fc6ddf00b8881b1812c95daa43
josedallatorre <jose.dallatorre@hotmail.com>
2025-08-27 17:25:48 +0200

    feat: added id for subreddit

--- src/main/java/com/example/redditvault/subreddit/Subreddit.java
+++ src/main/java/com/example/redditvault/subreddit/Subreddit.java
@@ -3,21 +3,27 @@ package com.example.redditvault.subreddit;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table
 public class Subreddit {
     @Id
+    private String subredditId;
+
     private String name;
 
     public Subreddit() {
     }
-    public Subreddit(String name) {
-        this.name = name;
+    public Subreddit(String subredditId) {
+        this.subredditId = subredditId;
     }
 
-    public String getName() {
-        return name;
+    public void setSubredditId(String subredditId) {
+        this.subredditId = subredditId;
     }
 
     public void setName(String name) {
