5db18b2ae8da85e0bc7c697a273c36801345c2aa
josedallatorre <jose.dallatorre@hotmail.com>
2025-09-18 21:29:42 +0200

    fix: using dto

--- src/main/java/com/example/redditvault/client/SingleImageExtractor.java
+++ src/main/java/com/example/redditvault/client/SingleImageExtractor.java
@@ -1,17 +1,23 @@
 package com.example.redditvault.client;
 
-import com.fasterxml.jackson.databind.JsonNode;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class SingleImageExtractor implements MediaExtractor {
     @Override
-    public boolean supports(JsonNode postData) {
-        return postData.has("url_overridden_by_dest");
+    public boolean supports(RedditSavedItem post) {
+        return post.getIsRedditMediaDomain() && post.getUrl_overridden_by_dest() != null;
     }
 
     @Override
-    public List<String> extractMediaUrls(JsonNode postData) {
-        return List.of(postData.get("url_overridden_by_dest").asText());
+    public List<String> extractMediaUrls(RedditSavedItem post) {
+        List<String> urls = new ArrayList<>();
+        String url = post.getUrl_overridden_by_dest();
+        int i = url.indexOf("?source=fallback");
+        if (i != -1) {
+            url = url.substring(0, i);
+        }
+        urls.add(url);
+        return urls;
     }
 }
