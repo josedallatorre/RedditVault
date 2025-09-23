4235654b46e545d1429f3422247084063b25ae54
josedallatorre <jose.dallatorre@hotmail.com>
2025-09-23 23:48:51 +0200

    fix: unused entity

--- src/main/java/com/example/redditvault/redditPost/RedditPost.java
+++ src/main/java/com/example/redditvault/redditPost/RedditPost.java
@@ -1,9 +1,5 @@
 package com.example.redditvault.redditPost;
-import com.example.redditvault.client.Media;
 import jakarta.persistence.*;
-import java.util.List;
-
-
 
 @Entity
 @Table
@@ -15,9 +11,6 @@ public class RedditPost {
     private String url;
     private String subredditId;
     private String redditUsername;
-
-    @OneToMany(mappedBy = "redditPost", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Media> mediaList;
 
     public RedditPost() {}
 
@@ -80,13 +73,6 @@ public class RedditPost {
         this.redditUsername = redditUsername;
     }
 
-    public List<Media> getMediaList() {
-        return mediaList;
-    }
-
-    public void setMediaList(List<Media> mediaList) {
-        this.mediaList = mediaList;
-    }
 
     @Override
     public String toString() {
