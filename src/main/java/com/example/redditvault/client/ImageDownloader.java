d885e118b437b5991cffdbd0c425aff45a8265c0
josedallatorre <jose.dallatorre@hotmail.com>
2025-09-26 23:56:37 +0200

    WIP modifying logic 4

--- src/main/java/com/example/redditvault/client/ImageDownloader.java
+++ src/main/java/com/example/redditvault/client/ImageDownloader.java
@@ -6,8 +6,8 @@ import java.io.IOException;
 
 public class ImageDownloader implements MediaDownloader {
     @Override
-    public boolean supports(String url){
-        return url.contains("i.redd.it");
+    public boolean supports(RedditSavedItem post){
+        return post.getUrl_overridden_by_dest().startsWith("https://i.redd.it/");
     }
     @Override
     public void download(String url) throws IOException {
