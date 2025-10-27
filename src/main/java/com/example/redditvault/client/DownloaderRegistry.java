da08782761743a2de2a5d3bc5ba30c49c0940629
josedallatorre <jose.dallatorre@hotmail.com>
2025-10-27 23:58:34 +0100

    adding TODO

--- src/main/java/com/example/redditvault/client/DownloaderRegistry.java
+++ src/main/java/com/example/redditvault/client/DownloaderRegistry.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class DownloaderRegistry {
     private final List<MediaDownloader> downloader = List.of(
+        //TODO: add media extarnal to Reddit (e.g., Imgur, Gfycat, etc.)
             new ImageDownloader(),
             new VideoDownloader(),
             new GalleryDownloader()
