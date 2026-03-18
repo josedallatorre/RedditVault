6dd6db5f363c9f305a435220ce29af70334b6cb4
josedallatorre <jose.dallatorre@hotmail.com>
2026-03-18 22:39:10 +0100

    added TODO

--- src/main/java/com/example/redditvault/client/RedditTokenRepository.java
+++ src/main/java/com/example/redditvault/client/RedditTokenRepository.java
@@ -8,5 +8,6 @@ import java.util.Optional;
 @Repository
 public interface RedditTokenRepository extends JpaRepository<RedditToken, Long> {
     Optional<RedditToken> findByRedditUsername(String redditUsername);
+    Optional<RedditToken> findByRedditVaultToken(String redditVaultToken);
 }
 
