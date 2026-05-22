2e95599f3e135acfc1e38abb089ff8c199f99ae8
josedallatorre <jose.dallatorre@hotmail.com>
2026-05-22 19:27:26 +0200

    fix:typo

--- src/main/java/com/example/redditvault/client/DownloaderRegistry.java
+++ src/main/java/com/example/redditvault/client/DownloaderRegistry.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class DownloaderRegistry {
     private final List<MediaDownloader> downloader = List.of(
-        //TODO: add media extarnal to Reddit (e.g., Imgur, Gfycat, etc.)
+        //TODO: add media external to Reddit (e.g., Imgur, Gfycat, etc.)
             new ImageDownloader(),
             new VideoDownloader(),
             new GalleryDownloader()
